848c44b61dbe41e39f4995389d8f6e59c424c8bf
muhammedfurkanfiruz <mfurkan.firuz@gmail.com>
2021-09-14 22:25:54 +0300

    ja was added

---
+++ main.js
@@ -0,0 +1,23 @@
+document.getElementById("submit").addEventListener("click", handler9);
+let vocabEnglish = [];
+function handler9()  {
+
+  
+    let englishWords = document.getElementById("input").innerText
+
+
+    for (let i = 0; i < englishWords.length; i++) {
+            let englishWord = englishWords[i].innerText;
+            vocabEnglish.push(englishWord); 
+        
+        }
+        console.log("çalıştım");
+        console.log(englishWords);
+        return vocabEnglish;
+      
+    }
+console.log(handler9());
+console.log(vocabEnglish);
+
+
+            
