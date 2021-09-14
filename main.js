37f78df44437163e64cce5168d2d4c73d981269b
muhammedfurkanfiruz <mfurkan.firuz@gmail.com>
2021-09-14 22:58:42 +0300

    getting value pushed to array

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ bosDizi = [];
 function doFunction(){
   
     const val = document.getElementById('exampleFormControlTextarea1').value;
-  console.log(val);
+    bosDizi.push(val)
+    console.log(bosDizi);
+    return bosDizi;
+
+ 
 }
+
+
+
             
