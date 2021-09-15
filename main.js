8669e38abd5e90442b8e9305e1fd5b3a7b18bead
muhammedfurkanfiruz <mfurkan.firuz@gmail.com>
2021-09-15 14:21:30 +0300

    getting values from an array and creating list

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
-bosDizi = [];
-function doFunction(){
+// bosDizi = [];
+// function doFunction(){
   
-    const val = document.getElementById('exampleFormControlTextarea1').value;
-    bosDizi.push(val)
-    console.log(bosDizi);
-    return bosDizi;
+//     const val = document.getElementById('exampleFormControlTextarea1').value;
+//     bosDizi.push(val)
+//     console.log(bosDizi);
+//     return bosDizi;
 
  
-}
+// }
 
+const isimler = ['alpiş baba ', 'meheme', 'furkan', 'saidoo', 'umuut'];  // ı just vreated an array 
+
+let html = ''; // setup the html tags
+
+isimler.forEach(function(isim){
+    html += '<li>' + isim + '</li>';      //with this function we get all value of the array and add li tag for each return(step)
+    
+
+});
+ 
+    html = (`<ul>  ${html}  <ul> `); // we putt the li tags in to the ul tags ı prefered to use template literal structure
+
+    console.log(html);
+    document.getElementById('isimYaz').innerHTML = html   // by using inner html we send the unordered list to the html 
+                                                          // because we wanna see the result on the browser
 
 
             
