25b696b9f71cef2ddec6ef61cef1145e3fa52729
muhammedfurkanfiruz <mfurkan.firuz@gmail.com>
2021-09-18 20:39:18 +0300

    Keyboard events was added

--- main.js
+++ main.js
@@ -1,25 +1,52 @@
 const todoList = [];
+let todoInput = document.getElementById('todoInput');
+let submitBtn = document.getElementById("submitBtn");
 
 function getValueFromInput() {
-    let todoInput = document.getElementById('todoInput');
     let val = todoInput.value;
+    if (val == '')  {
+        alert("empty" ); // alert
+    } 
+    if(val !== ''){
     todoList.push(val)
-    let list = document.getElementById('todoList')
+    let list = document.getElementById('todoList');
     let li = document.createElement("li");
     li.appendChild(document.createTextNode(val));
     list.appendChild(li);
-
-    console.log(todoList);
     todoInput.value = ""; //input clear
+    console.log(todoList);
+    }
 }
+
+todoInput.addEventListener("keyup", function(event)  { //add todo with enter key
+    event.preventDefault();
+    if (event.key === 'Enter' ) {
+        submitBtn.click();
+    }
+});
+
 
 //saklanan veriyi sonradan basmak için kullancaz
 // let html = '';
 
 // todoList.forEach(function (name) {
 //     html += '<li>' + name + '</li>';
+//     return html;
 // });
+
 
 // html = (`<ul>  ${html}  <ul> `);
 // console.log(html);
-// document.getElementById('name_ıd').innerHTML = html
+// document.getElementById('nameId').innerHTML = html
+
+
+
+
+
+
+
+
+   
+
+
+
