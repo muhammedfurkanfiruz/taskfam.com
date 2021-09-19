e2293f51c887a7f1d477eeb6564c91a2d01614f6
muhammedfurkanfiruz <mfurkan.firuz@gmail.com>
2021-09-19 14:38:18 +0300

    local storage and uniq ids are added

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-const todoList = [];
+var  todoList = [];
+getLocalStorage();
 let todoInput = document.getElementById('todoInput');
 let submitBtn = document.getElementById("submitBtn");
 
 function getValueFromInput() {
-    let val = todoInput.value;
+    let val =  todoInput.value;
     if (val == '')  {
         toastr.warning("Boş ekleme yapılamaz!");
     } 
     
     if(val !== ''){
-    todoList.push(val)
+    var uniq = 'id' + (new Date()).getTime();
+    todoList.push({'id' : uniq, 'text': val })
+    setLocalStorage();
     let list = document.getElementById('todoList');
     let li = document.createElement("li");
     li.appendChild(document.createTextNode(val));
@@ -17,8 +20,13 @@ function getValueFromInput() {
     toastr.success( 'Ekleme başarılı ! ');
     todoInput.value = ""; //input clear
     console.log(todoList);
+ 
     }
+  
 }
+
+
+
 
 todoInput.addEventListener("keyup", function(event)  { //add todo with enter key
     event.preventDefault();
@@ -26,20 +34,38 @@ todoInput.addEventListener("keyup", function(event)  { //add todo with enter key
         submitBtn.click();
     }
 });
+//  var data = [ {'Id' : 0,'text' : 'Alperen', },{  'Id' : 5,'text' : 'Alpiş', }];
+function setLocalStorage(){
+    localStorage.setItem('TaskList', JSON.stringify(todoList)); // setting the data to the local storage
+  
+}
+function getLocalStorage(){
+
+ 
+   let  gettingval =  localStorage.getItem('TaskList');
+   if(gettingval !== null ){
+   let veri = JSON.parse(gettingval)
+   todoList = veri 
+   console.log(todoList);
+
+   let html = '';
+
+todoList.forEach(function (name) {
+    html += '<li>' + name + '</li>';
+    return html;
+
+} ) ;
 
 
-//saklanan veriyi sonradan basmak için kullancaz
-// let html = '';
-
-// todoList.forEach(function (name) {
-//     html += '<li>' + name + '</li>';
-//     return html;
-// });
+//html = (`<ul>  ${html}  <ul> `);
+console.log(html);
+document.getElementById('todoList').innerHTML = html
+}}
 
 
-// html = (`<ul>  ${html}  <ul> `);
-// console.log(html);
-// document.getElementById('nameId').innerHTML = html
+
+
+
 
 
 
